eb96ba512396cc5f0a42dfc0d69a94a5bf63b944
Adeeb Mohammed <adeeb2358@gmail.com>
2020-05-01 04:07:01 +0900

    single cycle check

--- SIngleCycleCheck/main.cpp
+++ SIngleCycleCheck/main.cpp
@@ -4,12 +4,34 @@
 //
 //  Created by adeeb mohammed on 01/05/20.
 //  Copyright © 2020 adeeb mohammed. All rights reserved.
-//
+// program to find whether an array has a single cycle
 
 #include <iostream>
+#include <vector>
+
+int getNextIndex(int currentIndex, std::vector<int> &array){
+    int nextNumber = array.at(currentIndex);
+    int nextIdx = (nextNumber+currentIndex) % (int)array.size();
+    return  nextIdx >= 0 ? nextIdx : (int)array.size() + nextIdx;
+}
+
+
+bool hasSingleCycle(std::vector<int> array) {
+    int numVisits = 0;
+    int currentIndex = 0;
+    while (numVisits < array.size()) {
+        if (numVisits > 0 && currentIndex == 0) {
+            return false;
+        }
+        currentIndex = getNextIndex(currentIndex, array);
+        numVisits++;
+    }
+    return currentIndex == 0;
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Single Cycle check in an array" << std::endl;
+    std::cout << hasSingleCycle({2,4,5,-1,6});
     return 0;
 }
